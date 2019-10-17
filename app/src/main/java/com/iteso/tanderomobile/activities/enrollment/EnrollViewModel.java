f153ad84735829b0be9290360554bb60ac622cb9
Isaac <isaaccabrera31@gmail.com>
2019-10-16 19:25:02 -0500

    Write every new user in the database.

--- app/src/main/java/com/iteso/tanderomobile/activities/enrollment/EnrollViewModel.java
+++ app/src/main/java/com/iteso/tanderomobile/activities/enrollment/EnrollViewModel.java
@@ -1,5 +1,7 @@
 package com.iteso.tanderomobile.activities.enrollment;
 
+import android.provider.ContactsContract;
+
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
@@ -8,10 +10,17 @@ import androidx.lifecycle.ViewModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 import com.iteso.tanderomobile.repositories.authentication.AuthenticationManager;
+import com.iteso.tanderomobile.repositories.database.DatabaseManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EnrollViewModel extends ViewModel {
     private AuthenticationManager auth = AuthenticationManager.createInstance();
+    private DatabaseManager dbManager = DatabaseManager.createInstance();
+
     private MutableLiveData<Boolean> createAccountStatus = new MutableLiveData<>();
 
     public LiveData<Boolean> getCreateAccountStatus() {
@@ -31,4 +40,22 @@ public class EnrollViewModel extends ViewModel {
                     }
                 });
     }
+
+    public FirebaseUser getCurrentUser() {
+        return auth.getCurrentUser();
+    }
+
+    public void writeUserInDatabase(String email) {
+        Map<String, Object> user = new HashMap<>();
+        user.put("isActivated",true);
+        user.put("isPrivate",true);
+        user.put("ratingOrganizador",5);
+        user.put("ratingParticipante",5);
+        user.put("tandasOwned",0);
+
+        dbManager.getCollectionRef("users")
+                    .document(email)
+                    .set(user);
+    }
+
 }
